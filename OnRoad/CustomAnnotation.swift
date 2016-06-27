537120a2fa716065ac7278452c7e0ac15de3fbca
Administrator <admin@example.com>
2016-06-28 00:19:01 +0900

    custom annotation 作った

---
+++ OnRoad/CustomAnnotation.swift
@@ -0,0 +1,16 @@
+//
+//  CustomAnnotation.swift
+//  OnRoad
+//
+//  Created by 冨平準喜 on 2016/06/27.
+//  Copyright © 2016年 冨平準喜. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class CustomAnnotaion: MKPointAnnotation {
+    
+    var image:UIImage?
+    
+}
