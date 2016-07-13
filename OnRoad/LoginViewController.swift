1730a795a545ffa7394ac2b8253d739b3006c045
toshiki tomihira <toshiki0714@icloud.com>
2016-07-14 00:55:31 +0900

    ログイン，サインアップ画面の実装完了

--- OnRoad/LoginViewController.swift
+++ OnRoad/LoginViewController.swift
@@ -25,6 +25,9 @@ class LoginViewController: UITableViewController, UITextFieldDelegate{
         self.navigationItem.leftBarButtonItem = backbutton
         
     }
+    override func viewWillAppear(animated: Bool) {
+        self.usernameField.becomeFirstResponder()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,5 +38,81 @@ class LoginViewController: UITableViewController, UITextFieldDelegate{
     }
     func backHome(sender:UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //    TextFieldのリターンを押した処理
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if textField == usernameField {
+            passwordField.becomeFirstResponder()
+        }else if textField == passwordField {
+            if checkField()
+            {
+                login()
+            }
+        }
+        
+        return true
+    }
+    func checkStr(str:String?) -> Bool
+    {
+        if str == nil{
+            return false
+        }
+        
+        var result:Bool = true
+        let characters = str!.characters.map { String($0) }
+        
+        for c in characters
+        {
+            if Int(c) == nil && c.uppercaseString == c.lowercaseString
+            {
+                result = false
+            }
+        }
+        
+        return result
+    }
+    
+    func checkField() -> Bool
+    {
+        if self.usernameField.text?.utf16.count >= 3 && self.usernameField.text?.utf16.count <= 15 && checkStr(usernameField.text){
+            
+            if passwordField.text?.utf16.count >= 4 && passwordField.text?.utf16.count <= 16 && checkStr(passwordField.text){
+                
+                return true
+            }else{
+                AlertHelper.showOkAlert("エラー", message: "パスワードは半角英数字で4文字以上、16文字以下にしてください．", parent: self)
+                return false
+            }
+        }else{
+            AlertHelper.showOkAlert("エラー", message: "ユーザー名は半角英数字で3から15文字以内です．", parent: self)
+            return false
+        }
+    }
+    
+    func login()
+    {
+        let userModel = UserModel()
+        userModel.checkUserId2(usernameField.text!, password: passwordField.text!, callback: {result in
+            if result == true
+            {
+                AlertHelper.showAlert("ログイン", message: "ログインが成功しました！", cancel: "ok", destructive: nil, others: nil, parent: self){
+                    (buttonIndex: Int) in
+                    
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                    let notification : NSNotification = NSNotification(name: "Login", object: self, userInfo: nil)
+                    NSNotificationCenter.defaultCenter().postNotification(notification)
+                }
+            }
+            else if result == false
+            {
+                AlertHelper.showOkAlert("エラー", message: "ユーザー名またはパスワードが間違っています．", parent: self)
+            }
+            else
+            {
+                AlertHelper.showOkAlert("エラー", message: "インターネットに接続されていない可能性があります．", parent: self)
+            }
+        })
     }
 }
---
+++ OnRoad/UserModel.swift
@@ -0,0 +1,115 @@
+//
+//  UserModel.swift
+//  OnRoad
+//
+//  Created by 冨平準喜 on 2016/07/13.
+//  Copyright © 2016年 冨平準喜. All rights reserved.
+//
+
+import Foundation
+import NCMB
+
+class UserModel: NSObject {
+    
+    var userName:String?
+    var mail:String?
+    var phoneNumber:String?
+    var carType:String?
+    var password:String?
+    
+    
+    func checkUserId(name:String,callback:Bool? -> Void)
+    {
+        let query = NCMBQuery(className: "User")
+        query.whereKey("UserName", equalTo: name)
+        
+        query.findObjectsInBackgroundWithBlock({(objects, error) in
+            if error != nil {
+                print("error")
+                callback(nil)
+                
+            }else{
+                print(objects)
+                if objects.count == 0
+                {
+                    callback(false)
+                }
+                else
+                {
+                    callback(true)
+                }
+            }
+        })
+    }
+    
+    func checkUserId2(name:String,password:String,callback:Bool? -> Void)
+    {
+        let query = NCMBQuery(className: "User")
+        query.whereKey("UserName", equalTo: name)
+        
+        query.findObjectsInBackgroundWithBlock({(objects, error) in
+            if error != nil {
+                print("error")
+                
+            }else{
+                print(objects)
+                if objects.count == 1 && objects.first?.objectForKey("Password") as? String == password
+                {
+                    UserDefaults.userId = objects.first?.objectId
+                    callback(true)
+                }
+                else
+                {
+                    callback(false)
+                }
+            }
+        })
+    }
+    
+    func registration(callback:Bool -> Void)
+    {
+        if (userName == nil) || (mail == nil) || (phoneNumber == nil) || (carType == nil) || (password == nil)
+        {
+            callback(false)
+            return
+        }
+        let obj2 = NCMBObject(className: "User")
+        
+        // オブジェクトに値を設定
+        obj2.setObject(userName, forKey: "UserName")
+        obj2.setObject(mail, forKey: "Mail")
+        obj2.setObject(phoneNumber, forKey: "PhoneNumber")
+        obj2.setObject(carType, forKey: "CarType")
+        obj2.setObject(password, forKey: "Password")
+        
+        // データストアへの保存を実施
+        obj2.saveEventually { (error: NSError!) -> Void in
+            if error != nil {
+                // 保存に失敗した場合の処理
+                print("error")
+                callback(false)
+            }else{
+                // 保存に成功した場合の処理
+                print("save success")
+                
+                
+                self.checkUserId2(self.userName!,password: self.password!,callback: {result in
+                    if result == true
+                    {
+                        UserDefaults.userName = self.userName
+                        UserDefaults.mail = self.mail
+                        UserDefaults.phoneNumber = self.phoneNumber
+                        UserDefaults.carType = self.carType
+                        callback(true)
+                    }
+                    else
+                    {
+                        callback(false)
+                    }
+                })
+                
+                
+            }
+        }
+    }
+}
